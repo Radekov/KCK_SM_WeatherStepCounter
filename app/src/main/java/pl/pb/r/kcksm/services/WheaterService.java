57d319f69935283addf624b7787d8cd8e777b63b
Radosław Naruszewicz <radoslaw@qburst.com>
2016-11-26 21:10:25 +0100

    Added WeatherService.

--- app/src/main/java/pl/pb/r/kcksm/services/WheaterService.java
+++ app/src/main/java/pl/pb/r/kcksm/services/WheaterService.java
@@ -1,19 +1,22 @@
 package pl.pb.r.kcksm.services;
 
+import android.app.IntentService;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
 
 import org.json.JSONException;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import pl.pb.r.kcksm.MainActivity;
 import pl.pb.r.kcksm.http.WeatherHttpClient;
 import pl.pb.r.kcksm.model.Weather;
 import pl.pb.r.kcksm.wheater.JSONWeatherParse;
 
-public class WheaterService extends Service {
+public class WheaterService extends IntentService {
 
     private Weather weather;
     private Map<String, Float> coordinate = new HashMap<String, Float>();
@@ -21,10 +24,20 @@ public class WheaterService extends Service {
     public static final String EXTRA_LONGITUDE = "EXTRA_LONGITUDE";
     public static final String EXTRA_LATITUDE = "EXTRA_LATITUDE";
 
+    public WheaterService() {
+        super("WheaterService");
+    }
+
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
+        //throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        sendWeather(getWeather(intent));
     }
 
     @Override
@@ -37,21 +50,20 @@ public class WheaterService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        sendWeather(getWeather(intent));
+
+        //return super.onStartCommand(intent, flags, startId);
+        return START_NOT_STICKY;
+    }
+
+    private Weather getWeather(Intent intent){
         float lon = intent.getFloatExtra(EXTRA_LONGITUDE,coordinate.get("lon"));
         float lat = intent.getFloatExtra(EXTRA_LATITUDE,coordinate.get("lat"));
 
         coordinate.put("lon",lon);
         coordinate.put("lat",lat);
 
-        //TODO return to MainActivity
-        getWeather();
-
-        //return super.onStartCommand(intent, flags, startId);
-        return START_NOT_STICKY;
-    }
-
-    private Weather getWeather(){
-        Weather weather = new Weather();
+        weather = new Weather();
         String data = ((new WeatherHttpClient()).getWeatherData(coordinate.get("lat"), coordinate.get("lon")));
         try {
             weather = JSONWeatherParse.getWeather(data);
@@ -59,6 +71,12 @@ public class WheaterService extends Service {
             e.printStackTrace();
         }
 
-        return null;
+        return weather;
+    }
+    private boolean sendWeather(Weather w){
+        Intent intent = new Intent("NewWeather");
+        intent.putExtra(MainActivity.EXTRA_WEATHER,w);
+        //sendBroadcast(intent2);
+        return LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
     }
 }
